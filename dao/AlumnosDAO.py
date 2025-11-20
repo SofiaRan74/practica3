98ffb0d2bc6a9094dc4180af4b66dfde0bf8d3c7
SofiaRan74 <sofiaran024@gmail.com>
2025-11-19 20:13:51 -0600

    Create AlumnosDAO.py

---
+++ dao/AlumnosDAO.py
@@ -0,0 +1,8 @@
+def obtener_alumnos():
+    con = DatabaseConnection().get_connection()
+    cursor = con.cursor(dictionary=True)
+
+    cursor.execute("SELECT idAlumno, NombreCompleto FROM alumnos ORDER BY NombreCompleto ASC")
+    registros = cursor.fetchall()
+    cursor.close()
+    return registros
