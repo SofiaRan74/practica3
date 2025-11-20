2603ccdb7fbd05b3bca5f59d46d7ae6dcc1492c6
SofiaRan74 <sofiaran024@gmail.com>
2025-11-19 20:38:36 -0600

    Update AlumnosDAO.py

--- dao/AlumnosDAO.py
+++ dao/AlumnosDAO.py
@@ -1,8 +1,11 @@
-def obtener_alumnos():
-    con = DatabaseConnection().get_connection()
-    cursor = con.cursor(dictionary=True)
+from dao.DatabaseConnection import DatabaseConnection
 
-    cursor.execute("SELECT idAlumno, NombreCompleto FROM alumnos ORDER BY NombreCompleto ASC")
-    registros = cursor.fetchall()
-    cursor.close()
-    return registros
+class AlumnosDAO:
+    def obtener_alumnos():
+        con = DatabaseConnection().get_connection()
+        cursor = con.cursor(dictionary=True)
+    
+        cursor.execute("SELECT idAlumno, NombreCompleto FROM alumnos ORDER BY NombreCompleto ASC")
+        registros = cursor.fetchall()
+        cursor.close()
+        return registros
